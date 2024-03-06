abe83c00beed281d9954de361420d58ea4683d9b
josh-133 <joshuaburge507@gmail.com>
2024-03-06 04:27:02 +0000

    feat: finally created the initial angular app to try and render html on the frontend instead of python FastAPI. However, I am having issues with the HttpClient.

--- angular/angular-app/src/app/app.component.ts
+++ angular/angular-app/src/app/app.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { BrowserModule } from '@angular/platform-browser';
+import { HttpClient } from '@angular/common/http';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
@@ -8,6 +11,22 @@ import { RouterOutlet } from '@angular/router';
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  
+  configuration: any;
+  presetsArray: any;
+  optionsArray: any;
+
   title = 'angular-app';
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit(): void {
+    this.http.get<any>('http://localhost:5000/').subscribe(data => {
+      this.configuration = data.configuration;
+      this.presetsArray = data.presetsArray;
+      this.optionsArray = data.optionsArray;
+    });
+  }
+
 }
---
+++ angular/angular-app/src/app/app.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http'; // Import HttpClientModule
+import { AppComponent } from './app.component';
+import { CommonModule } from '@angular/common';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    CommonModule,
+    BrowserModule,
+    HttpClientModule // Add HttpClientModule to imports
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
